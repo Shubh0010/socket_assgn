a2556084c9971abc23aa65dd2eaa403f948eb944
shubh0010 <shubhamnegi0010@gmail.com>
2019-02-21 14:49:36 +0530

    socket first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require(`express`)
+const app = express()
+const socketio = require('socket.io')
+
+users = []
+connections = []
+const io = socketio(app.listen(4001, (err, data) => {
+  console.log("server running")
+}))
+
+app.get(`/`, (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+io.on(`connection`, (socket) => {
+  connections.push(socket)
+  
+  //disconnect
+  socket.on(`disconnect`, (data) => {
+    users.splice(users.indexOf(socket.username),1)
+    updateUserNames()
+    connections.splice(connections.indexOf(socket), 1)
+  })
+
+  //send message
+
+  socket.on(`send message`,(data)=>{
+    io.emit(`new message`, {msg : data, user: socket.username} )
+  })
+
+  //new user
+
+  socket.on(`new user`,(data,callback)=>{
+    callback(true)
+    socket.username = data;
+    users.push(socket.username);
+    updateUserNames();
+  })
+
+  function updateUserNames(){
+    io.emit(`get users`, users)
+  }
+})
