9d848eca56032f71bbe1d408ea554920d7977cb9
shubh0010 <shubhamnegi0010@gmail.com>
2019-02-27 12:05:28 +0530

    test0

--- server.js
+++ server.js
@@ -1,49 +1,45 @@
-const express = require(`express`);
-const app = express();
-const socketio = require("socket.io");
+const express = require(`express`)
+const app = express()
+const socketio = require('socket.io')
 
-users = [];
-connections = [];
-const io = socketio(
-  app.listen(4001, (err, data) => {
-    console.log("server running");
-  })
-);
+users = []
+connections = []
+const io = socketio(app.listen(4001, (err, data) => {
+  console.log("server running")
+}))
 
-const admin = io.of("/admin");
+const admin = io.of("/admin")
 app.get(`/`, (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+  res.sendFile(__dirname + '/public/index.html')
+})
 
-io.on(`connection`, socket => {
-  connections.push(socket);
+
+io.on(`connection`, (socket) => {
+  connections.push(socket)
 
   //disconnect
-  socket.on(`disconnect`, data => {
-    users.splice(users.indexOf(socket.username), 1);
-    updateUserNames();
-    connections.splice(connections.indexOf(socket), 1);
-  });
+  socket.on(`disconnect`, (data) => {
+    users.splice(users.indexOf(socket.username), 1)
+    updateUserNames()
+    connections.splice(connections.indexOf(socket), 1)
+  })
 
   //send message
 
-  socket.on(`send message`, data => {
-    io.emit(`new message`, {
-      msg: data,
-      user: socket.username
-    });
-  });
+  socket.on(`send message`, (data) => {
+    io.emit(`new message`, { msg: data, user: socket.username })
+  })
 
   //new user
 
   socket.on(`new user`, (data, callback) => {
-    callback(true);
+    callback(true)
     socket.username = data;
     users.push(socket.username);
     updateUserNames();
-  });
+  })
 
   function updateUserNames() {
-    io.emit(`get users`, users);
+    io.emit(`get users`, users)
   }
-});
+})
